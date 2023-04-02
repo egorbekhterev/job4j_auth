18bd323eed74bbe79c1446cc2cf5a912fa444900
Paranoj <e.behterev@yandex.ru>
2023-04-02 23:54:52 +0700

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/ru/job4j/auth/filter/JWTAuthorizationFilter.java
@@ -70,6 +70,7 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
         if (header == null || !header.startsWith(TOKEN_PREFIX)) {
             chain.doFilter(request, response);
+            return ;
         }
 
         UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
--- src/test/java/ru/job4j/auth/AuthApplicationTests.java
+++ src/test/java/ru/job4j/auth/AuthApplicationTests.java
@@ -2,8 +2,10 @@ package ru.job4j.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class AuthApplicationTests {
 
 	@Test
